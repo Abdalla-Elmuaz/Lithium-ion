22ec39670081538ca6c984bd7476b4d50814eff8
Abdalla-Elmuaz <abdallaelmuaz2001@gmail.com>
2025-05-10 23:09:58 +0300

    Update Enn.py

--- PythonProject3/Enn.py
+++ PythonProject3/Enn.py
@@ -151,17 +151,18 @@ def run():
     ], axis=1)
         prediction = modellasso_en.predict(input_data)
         st.success(f"Predicted Degradation Rate: {prediction[0]:.4f}")
-         prediction_data = input_data.copy()
-         prediction_data['Predicted Enhanced Degradation Rate'] = prediction[0]
-         file_path = "predictions.csv"
-         if os.path.exists(file_path):
-             prediction_data.to_csv(file_path, mode='a', header=False, index=False)
-         else:
-             prediction_data.to_csv(file_path, mode='w', header=True, index=False)
+        prediction_data = input_data.copy()
+        prediction_data['Predicted Enhanced Degradation Rate'] = prediction[0]
+        file_path = "predictions.csv"
+        if os.path.exists(file_path):
+            
+            prediction_data.to_csv(file_path, mode='a', header=False, index=False)
+        else:
+            prediction_data.to_csv(file_path, mode='w', header=True, index=False)
 
         # Allow user to download the predictions file
-         with open(file_path, "rb") as f:
-             st.download_button("Download Predictions CSV", f, file_name="predictions.csv", mime="text/csv")
+        with open(file_path, "rb") as f:
+            st.download_button("Download Predictions CSV", f, file_name="predictions.csv", mime="text/csv")
 
 # Run the app
 if __name__ == "__main__":
