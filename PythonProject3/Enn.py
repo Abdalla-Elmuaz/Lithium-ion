2b15b647e50ba50a8d70a624877e2c8f46c9a1b1
Abdalla-Elmuaz <abdallaelmuaz2001@gmail.com>
2025-05-10 23:05:04 +0300

    Update Enn.py

--- PythonProject3/Enn.py
+++ PythonProject3/Enn.py
@@ -54,6 +54,9 @@ def run():
             display: inline-block;
             margin-top: 30px;
         }
+        div[data-testid="stDownloadButton"]:hover > button {
+        background-color: #222;
+    }
         </style>
         """,
         unsafe_allow_html=True
@@ -148,7 +151,20 @@ def run():
     ], axis=1)
         prediction = modellasso_en.predict(input_data)
         st.success(f"Predicted Degradation Rate: {prediction[0]:.4f}")
+         prediction_data = input_data.copy()
+        prediction_data['Predicted Enhanced Degradation Rate'] = prediction[0]
+        file_path = "predictions.csv"
+        if os.path.exists(file_path):
+            prediction_data.to_csv(file_path, mode='a', header=False, index=False)
+        else:
+            prediction_data.to_csv(file_path, mode='w', header=True, index=False)
+
+        # Allow user to download the predictions file
+        with open(file_path, "rb") as f:
+            st.download_button("Download Predictions CSV", f, file_name="predictions.csv", mime="text/csv")
 
 # Run the app
 if __name__ == "__main__":
     run()
+
+
